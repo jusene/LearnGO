ea06ce2897b735cf33938c9c9d5e765c3ccfeff7
jusene <710800324@qq.com>
2020-06-17 23:11:03 +0800

    day25

--- src/github.com/jusene/day20/sort4.go
+++ src/github.com/jusene/day20/sort4.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
-	s := []int{1, 21, 3, 4, 0}
+	s := []int{1, 21, 3, 4, 0, 10}
 	sort.Ints(s)
 	fmt.Println(sort.IntsAreSorted(s))
 	fmt.Println(s)
 	sort.Sort(sort.Reverse(sort.IntSlice(s)))
 	fmt.Println(s)
 	fmt.Println(sort.SearchInts(s, 21))
+
+	// 使用二分法算法来搜索某指定切片，如果slice以升序排序，则　f func中应该使用＞＝,如果slice以降序排序，则应该使用<=
+	fmt.Println(sort.Search(len(s), func(i int) bool {
+		return s[i] <= 1
+	}))
 
 	a := []string{"a", "b", "g", "d"}
 	sort.Strings(a)
